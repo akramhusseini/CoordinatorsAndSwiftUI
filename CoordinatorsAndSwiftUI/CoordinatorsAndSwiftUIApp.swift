51c28e8cebf13ebabf451c1f8ced6b93881d9bb9
akrammotory <93082320+akrammotory@users.noreply.github.com>
2024-07-22 15:08:47 +0300

    Initial Commit

---
+++ CoordinatorsAndSwiftUI/CoordinatorsAndSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorsAndSwiftUIApp.swift
+//  CoordinatorsAndSwiftUI
+//
+//  Created by Akram Husseini on 22/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorsAndSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
